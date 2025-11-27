7557b728061e0327868ef90ce48a405afce84e05
Princewill Tor-Anyiin <princewilltoranyiin@gmail.com>
2025-11-27 13:46:49 +0100

    refactor: Temporarily removed TonConnectUIProvider and ErrorBoundary from main.jsx for deeper debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import react from 'react';
-import { createRoot } from 'react-dom/client';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// Temporarily removed TonConnectUIProvider
+import { App } from './App.jsx'; // Make sure this is '{ App }'
+import './index.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-const manifestUrl = 'https://afro-nodedapp.netlify.app/tonconnect-manifest.json';
-
-root.render(
-  <react.StrictMode>
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <App />
-    </TonConnectUIProvider>
-  </react.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
