7a639cddd88034e8f6013255e6f61f0594eea3bd
Princewill Tor-Anyiin <princewilltoranyiin@gmail.com>
2025-11-28 16:27:54 +0100

    fix: Use default import for JSON config (manifestUrl fix)

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { manifestUrl } from '../tonconnect-dapp-config.json';
+// FIX: Changed from named import to default import + destructuring
+import config from '../tonconnect-dapp-config.json';
+const { manifestUrl } = config; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
