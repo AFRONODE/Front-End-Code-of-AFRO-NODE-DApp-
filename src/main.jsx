5a7f219da474ad2a47603facc59a0ce09ba42562
Princewill Tor-Anyiin <princewilltoranyiin@gmail.com>
2025-11-28 15:43:35 +0100

    fix: Final syntax corrections and manifest import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx';
-import './src/index.css'; 
+import App from './App.jsx';
+import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-
-// IMPORTANT: Using your Netlify site URL from the log
-const manifestUrl = 'https://afron-node.netlify.app/tonconnect-dapp-config.json'; 
+import { manifestUrl } from '../tonconnect-dapp-config.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl={manifestUrl} actionsConfiguration={{ twaReturnUrl: 'https://t.me/AfronoNode' }}>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
       <App />
     </TonConnectUIProvider>
   </React.StrictMode>,
