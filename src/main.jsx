f0bf45edc8e3dbf0703613dea0e9ded3d025ae37
Your Name <your-email@example.com>
2026-02-10 16:14:14 +0100

    Emergency: Force JSON content-type and CORS via netlify.toml

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,10 @@ import App from './App'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+const manifestUrl = "https://afro-nodeweb3gigeconomydapp.netlify.app/tonconnect-manifest.json";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <TonConnectUIProvider manifestUrl="https://afro-nodeweb3gigeconomydapp.netlify.app/tonconnect-manifest.json">
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
   </TonConnectUIProvider>
 )
