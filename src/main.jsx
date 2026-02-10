43b827d6ad6ff25a1bd0370e92f6d74a54e19085
Your Name <your-email@example.com>
2026-02-10 15:51:48 +0100

    FIX: Force manifest routing and disable strict mode for wallet init

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TonConnectUIProvider 
-      manifestUrl="https://afro-nodeweb3gigeconomydapp.netlify.app/tonconnect-manifest.json"
-      actionsConfiguration={{
-        twaReturnUrl: 'https://t.me/your_bot_user_name'
-      }}
-    >
-      <App />
-    </TonConnectUIProvider>
-  </React.StrictMode>,
+  <TonConnectUIProvider manifestUrl="https://afro-nodeweb3gigeconomydapp.netlify.app/tonconnect-manifest.json">
+    <App />
+  </TonConnectUIProvider>
 )
